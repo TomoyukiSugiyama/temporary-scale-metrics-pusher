c3708dc794101890057bc06f8594df36de2e8ec7
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-07-30 21:41:03 +0900

    add vars

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "flag"
+
+func main() {
+	var pushgatewayAddr string
+	var pushgatewayPort string
+	flag.StringVar(&pushgatewayAddr, "pushgateway-address", "localhost", "ip address for pushgateway")
+	flag.StringVar(&pushgatewayPort, "pushgateway-port", "9091", "port number for pushgateway")
+	flag.Parse()
+}

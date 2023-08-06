55e5e7a2d1d8640a04917f0416a1321cf839dd09
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-08-06 13:20:11 +0900

    init TemporaryScaleMetrics struct

--- metrics/push.go
+++ metrics/push.go
@@ -2,24 +2,36 @@ package metrics
 
 import (
 	prometheusv1 "github.com/prometheus/client_golang/api/prometheus/v1"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Metrics interface {
 	Push()
 }
 
-type metrics struct {
-	temporaryScaleGauge prometheus.GaugeVec
-	prometheusApi       prometheusv1.API
+type TemporaryScaleMetrics struct {
+	conditionId   string
+	conditionType string
+	duration      string
+}
+
+type pusher struct {
+	prometheusApi         prometheusv1.API
+	temporaryScaleMetrics TemporaryScaleMetrics
 }
 
 func NewMetrics(prometheusApi prometheusv1.API) Metrics {
+	tsm := TemporaryScaleMetrics{
+		conditionId:   "9-22",
+		conditionType: "training",
+		duration:      "9-22",
+	}
 
-	m := &metrics{prometheusApi: prometheusApi}
-	return m
+	p := &pusher{prometheusApi: prometheusApi,
+		temporaryScaleMetrics: tsm,
+	}
+	return p
 }
 
-func (m *metrics) Push() {
+func (p *pusher) Push() {
 
 }

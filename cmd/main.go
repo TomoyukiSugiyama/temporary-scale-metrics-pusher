75c61617f69aa14e8dbfd69d34146f317b6f9168
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2023-08-06 12:39:28 +0900

    setup metrics package

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"flag"
+
+	metricspkg "github.com/TomoyukiSugiyama/temporary-scale-metrics-pusher/metrics"
+	prometheusapi "github.com/prometheus/client_golang/api"
+	prometheusv1 "github.com/prometheus/client_golang/api/prometheus/v1"
+)
 
 func main() {
 	var pushgatewayAddr string
@@ -8,6 +14,14 @@ func main() {
 	flag.StringVar(&pushgatewayAddr, "pushgateway-address", "localhost", "ip address for pushgateway")
 	flag.StringVar(&pushgatewayPort, "pushgateway-port", "9091", "port number for pushgateway")
 	flag.Parse()
+
+	client, err := prometheusapi.NewClient(prometheusapi.Config{Address: pushgatewayUrl(pushgatewayAddr, pushgatewayPort)})
+	if err != nil {
+
+	}
+	api := prometheusv1.NewAPI(client)
+	pusher := metricspkg.NewMetrics(api)
+	pusher.Push()
 }
 
 func pushgatewayUrl(address string, port string) string {
